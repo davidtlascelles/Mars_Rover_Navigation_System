85817b0f6d93bb40d9ec6b7da066f667aa9f03de
davidlascelles <davidtlascelles@gmail.com>
2020-04-26 00:34:10 -0700

    Mission Simulator code started. Runs mission up to preliminary route established

---
+++ Mission Simulator.py
@@ -0,0 +1,32 @@
+import os
+import shutil
+import time
+
+location = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+filepath = os.path.join(location, 'downlink.txt')
+
+
+def coordinate_topography():
+    src_file = os.path.join(location, "coordinate_topography_map_downlink.txt")
+    dup = src_file + ".dup"
+    shutil.copy(src_file, dup)
+
+    dst_file = os.path.join(location, "coordinate_topography_map_downlink.txt.dup")
+    new_dst_file_name = os.path.join(location, "downlink.txt")
+    os.rename(dst_file, new_dst_file_name)
+
+
+time.sleep(2)
+dst = ["navigation\n", "coordinates\n", "mission control\n", "(749, 574, 1117.56)"]
+f = open("downlink.txt", "w")
+f.writelines(dst)
+f.close()
+
+time.sleep(5)
+rvr = ["navigation\n", "coordinates\n", "mps orbiter\n", "(1328, 823, 1101.89)"]
+f = open("downlink.txt", "w")
+f.writelines(rvr)
+f.close()
+
+time.sleep(5)
+coordinate_topography()
