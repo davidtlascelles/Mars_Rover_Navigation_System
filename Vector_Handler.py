8931a85002a710b34fe6c7f3990112ea610c49d3
davidlascelles <davidtlascelles@gmail.com>
2020-04-27 21:32:39 -0700

    Overhauled class to handle vectors as objects

--- Vector_Handler.py
+++ Vector_Handler.py
@@ -1,36 +1,40 @@
 import numpy
 
 
-class VectorHandler:
+class Vector:
 
-    def __init__(self):
-        pass
+    def __init__(self, start, end, three_dimensional=True):
+        self.__starting_coordinate = start
+        self.__ending_coordinate = end
 
-    @staticmethod
-    def magnitude(mag_vector):
+        self.vector = self.__make_vector(three_dimensional)
+        self.radian_heading = self.__heading()
+        self.cardinal_heading = self.__heading(False)
+        self.magnitude = self.__magnitude()
+
+    def __magnitude(self):
         """Accepts a vector parameter and returns magnitude"""
         mag = 0
-        for component in mag_vector:
-            mag += component ** 2
+        for component in self.vector:
+            mag += numpy.power(component, 2)
 
         mag = numpy.sqrt(mag)
 
         return mag
 
-    @staticmethod
-    def make_vector(rover_coordinates, destination_coordinates, two_dimensional=None):
+    def __make_vector(self, three_dimensions):
         """Accepts two points and returns a vector"""
         transformed_destination = []
-        if two_dimensional is True:
-            transformed_destination.append(destination_coordinates[0] - rover_coordinates[0])
-            transformed_destination.append(destination_coordinates[1] - rover_coordinates[1])
+        if three_dimensions is False or len(self.__starting_coordinate) == 2:
+            transformed_destination.append(self.__ending_coordinate[0] - self.__starting_coordinate[0])
+            transformed_destination.append(self.__ending_coordinate[1] - self.__starting_coordinate[1])
         else:
-            for index, element in enumerate(rover_coordinates):
-                transformed_destination.append(destination_coordinates[index] - rover_coordinates[index])
+            for index, element in enumerate(self.__starting_coordinate):
+                transformed_destination.append(self.__ending_coordinate[index] - self.__starting_coordinate[index])
 
         return transformed_destination
 
-    def heading(self, vector, radians=None):
+    def __heading(self, radians=True):
         """
         Finds the cardinal heading, represented as an integer corresponding with the 8 cardinal directions:
         E: 0, NE: 1, N: 2, NW: 3, W: 4, SW: 5, S: 6, SE: 7
@@ -38,8 +42,8 @@ class VectorHandler:
         :param radians: changes return type to heading in radians
         :return: returns an integer cardinal direction heading
         """
-        x = vector[0]
-        y = vector[1]
+        x = self.vector[0]
+        y = self.vector[1]
 
         heading = None
 
@@ -62,12 +66,11 @@ class VectorHandler:
         else:
             heading = (numpy.pi / 2) - numpy.arctan((x / y))
 
-        if radians is None:
-            return self.__cardinal_heading(heading)
-        return heading
+        if radians is True:
+            return heading
+        return self.__cardinal_heading()
 
-    @staticmethod
-    def __cardinal_heading(heading):
+    def __cardinal_heading(self):
         """
         Support method for heading(), looks up cardinal direction from radian value
         :param heading: radian heading value
@@ -83,19 +86,25 @@ class VectorHandler:
         NEE = 0.392699081698724
         NNE = 1.17809724509617
 
-        if NNE > heading > NEE:
+        if NNE > self.radian_heading > NEE:
             return 1  # NE
-        if NNW > heading > NNE:
+        if NNW > self.radian_heading > NNE:
             return 2  # N
-        if NWW > heading > NNW:
+        if NWW > self.radian_heading > NNW:
             return 3  # NW
-        if SWW > heading > NWW:
+        if SWW > self.radian_heading > NWW:
             return 4  # W
-        if SSW > heading > SWW:
+        if SSW > self.radian_heading > SWW:
             return 5  # SW
-        if SSE > heading > SSW:
+        if SSE > self.radian_heading > SSW:
             return 6  # S
-        if SEE > heading > SSE:
+        if SEE > self.radian_heading > SSE:
             return 7  # SE
-        if NEE > heading or heading > SEE:
+        if NEE > self.radian_heading or self.radian_heading > SEE:
             return 0  # E
+
+    def set_start(self, start):
+        self.__starting_coordinate = start
+
+    def set_end(self, end):
+        self.__ending_coordinate = end
