9f391b4ca8ded0843d0776c39026d824976ad832
davidlascelles <davidtlascelles@gmail.com>
2020-04-28 17:37:41 -0700

    Dumb rover drives now. It's just blind, but I'm sure that's fine

--- Onboard_Systems_Interface.py
+++ Onboard_Systems_Interface.py
@@ -1,56 +1,58 @@
-import Communication_Dispatch
-import Database
-import Vector_Handler
-import main
+import time
 
 
 class DriveInterface:
 
-    def __init__(self):
+    def __init__(self, database_object, comms_object):
+        self.db = database_object
+        self.comms = comms_object
+        self.destination_coordinate = None
         self.current_coordinate = None
-        self.destination_coordinate = main.destination
 
-        self.vector_buffer = [(self.current_coordinate, Vector_Handler.Vector())]
+        self.vector_buffer = []
 
     def drive_buffer(self, done=False):
-        db = Database.Database()
-        current = self.current_coordinate
-
-        visited = 0
-        while current != self.destination_coordinate:
-            v = 0
-            first = self.vector_buffer[0][0]
-            last = self.vector_buffer[len(self.vector_buffer) - 1][0]
-            if first != last:
-                v = Vector_Handler.Vector(first, last)
-
-            while v.magnitude < 10:
-                visited += 1
-
-                next_point = db.select_point_by_visited(visited)
-                if next_point == self.destination_coordinate:
-                    done = True
-                    break
-                while next_point is None:
-                    visited += 1
-                    next_point = db.select_point_by_visited(visited)
-                self.vector_buffer.append((next_point, Vector_Handler.Vector(current, next_point)))
-                current = next_point
-            if done is True:
-                break
+        # while current != self.destination_coordinate:
+        #     v = [0, 0]
+        #     first = self.vector_buffer[0][0]
+        #     last = self.vector_buffer[len(self.vector_buffer) - 1][0]
+        #     if first != last:
+        #         v = Vector_Handler.Vector(first, last)
+        #
+        #     while v.magnitude < 10:
+        #         visited += 1
+        #
+        #         next_point = db.select_point_by_visited(visited)
+        #         if next_point == self.destination_coordinate:
+        #             done = True
+        #             break
+        #         while next_point is None:
+        #             visited += 1
+        #             next_point = db.select_point_by_visited(visited)
+        #         self.vector_buffer.append((next_point, Vector_Handler.Vector(current, next_point)))
+        #         current = next_point
+        #     if done is True:
+        #         break
         return
 
     def __interrupt_drive_buffer(self):
-        comms = Communication_Dispatch.CommunicationDispatch()
         self.drive_buffer(done=True)
         self.vector_buffer = []
-        comms.uplink_rover_status("DRIVE_FAULT")
+        self.comms.uplink_rover_status("DRIVE_FAULT")
         return
 
     def get_current_coordinates(self):
-        comms = Communication_Dispatch.CommunicationDispatch()
-        comms.request_current_coordinates()
-        self.current_coordinate = comms.downlink_coordinates("mps orbiter")
+        self.comms.get_current_coordinates()
+        self.current_coordinate = self.comms.downlink_coordinates("mps orbiter")
+        return
+
+    @classmethod
+    def set_destination_coordinates(cls, dest):
+        cls.destination_coordinate = dest
+        return
+
+    def append_vector_buffer(self, vector):
+        self.vector_buffer.append(vector)
         return
 
     def get_sensor_vector(self):
@@ -59,24 +61,50 @@ class DriveInterface:
         return
 
     def drive(self):
-        db = Database.Database()
-        # Get first vector in buffer
-        vector = self.vector_buffer.pop(0)
+        self.comms.uplink_rover_status("GO_DRIVE")
+        self.current_coordinate = self.db.select_point_by_visited(1)
+        current = self.current_coordinate
+        self.db.create_traversed(current)
 
-        self.drive_buffer()
-        # Send vector to drive system
-        print("Send vector to drive system", vector[1])
-        # Wait for response
-        print("Wait for response")
-        response = None
-        if response == "Success":
-            # Update current coordinates
-            self.get_current_coordinates()  ########################################## Mission Sim needs to handle this
-            # Trigger imaging interface
-            self.get_sensor_vector()
-            # Add point to traversed
-            db.create_traversed(vector[0])
-            # Update buffer
+        visited = 0
+
+        while self.current_coordinate != self.destination_coordinate:
+            self.current_coordinate = self.downlink_coords()
+            print(self.current_coordinate)
+            self.db.create_traversed(self.current_coordinate)
+            print("Pass Vector To Imaging System") #########################################
+            print("Add Hazard Avoidance To waypoints") #####################################
             self.drive_buffer()
-        else:
-            self.__interrupt_drive_buffer()
+            print("Pass next vector to drive system")
+            print("Recieve drive system response")
+            response = None#####################################
+            while response is None:
+                time.sleep(5)
+                response = 0 ################################
+                if response == "Error":
+                    self.drive_buffer(done=True)
+                    self.comms.uplink_rover_status("DRIVE_FAULT")
+                    return
+                else:
+                    pass
+
+        self.comms.uplink_rover_status("SUCCESS")
+        return
+
+    def wait_for_comms_activity(self):
+        COMM_CHECK_INTERVAL = 1
+        activity = self.comms.activity()
+        while activity is False:
+            time.sleep(COMM_CHECK_INTERVAL)
+            activity = self.comms.activity()
+        return
+
+    def downlink_coords(self):
+        self.comms.get_current_coordinates()
+        self.wait_for_comms_activity()
+        point = self.comms.downlink_coordinates("mps orbiter")
+        if point is None:
+            self.comms.get_current_coordinates()
+            self.wait_for_comms_activity()
+            point = self.comms.downlink_coordinates("mps orbiter")
+        return point
